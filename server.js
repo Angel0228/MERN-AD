00fe187e2203f331dc73bf2d5a17f9d9a2b1e5dc
Angel0228 <supercode@Alexey.local>
2019-01-29 13:53:33 -0500

    heroku deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 //Routes Define
 const adverts = require("./routes/api/adverts");
@@ -26,6 +27,15 @@ mongoose
 app.use("/api/adverts", adverts);
 app.use("/api/users", users);
 app.use("/api/category", category);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5500;
 
